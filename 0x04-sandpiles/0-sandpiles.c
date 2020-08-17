d411c110c2242376f908d5218589033d17a2d653
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-08-17 15:41:10 -0500

    Task 0

---
+++ 0x04-sandpiles/0-sandpiles.c
@@ -0,0 +1,8 @@
+/**
+ * sandpiles_sum - function that computes the sum of two sandpiles
+ * @grid1: sandpile
+ * @grid2: sandpile
+ * Return: Nothing
+ */
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{}
