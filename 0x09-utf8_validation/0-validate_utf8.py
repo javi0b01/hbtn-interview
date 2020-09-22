a0a373432a4a82fee8f8177ac41726e9ecf6dcb5
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-09-22 18:21:11 -0500

    Task 0

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""
-Method that determines if a given data set represents a valid UTF-8 encoding.
-* Return: True if data is a valid UTF-8 encoding, else return False
-* A character in UTF-8 can be 1 to 4 bytes long
-* The data set can contain multiple characters
-* The data will be represented by a list of integers
-* Each integer represents 1 byte of data, therefore you only need to handle
-* the 8 least significant bits of each integer
+
+""" Method that determines if a given data set represents
+    a valid UTF-8 encoding.
+    * Return: True if data is a valid UTF-8 encoding, else return False
+    * A character in UTF-8 can be 1 to 4 bytes long
+    * The data set can contain multiple characters
+    * The data will be represented by a list of integers
+    * Each integer represents 1 byte of data, therefore you only need
+    * to handle the 8 least significant bits of each integer
 """
 
 
