9c8d2f16b69000df9682565ee1306d503789af27
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-11-17 13:19:39 -0500

    Task 0

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,13 +1,15 @@
 #!/usr/bin/node
-const request = require('request'),
-urlApi = 'https://swapi-api.hbtn.io/api/films/',
-movieId = process.argv[2];
-
+/* Star Wars Characters - Using the request module */
+const request = require('request');
+const urlApi = 'https://swapi-api.hbtn.io/api/films/';
+const movieId = process.argv[2];
+// query API
 request(urlApi + movieId, (error, response, body) => {
   if (error) throw error;
   const characters = JSON.parse(body).characters;
   showNames(characters);
 });
+// show results on the console
 const showNames = (names, i = 0) => {
   if (i === names.length) return;
   request(names[i], (error, response, body) => {
