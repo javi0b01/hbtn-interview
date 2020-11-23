0baf7747642849aef2696443bf6059d6fd8a1ab0
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-11-23 11:12:26 -0500

    Task 0

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-""" Rain - Given a list of non-negative integers representing walls of width 1,
-    calculate how much water will be retained after it rains. """
+""" Rain """
 
 
 def rain(walls):
-    """ walls is a list of non-negative integers.
+    """ Given a list of non-negative integers representing walls of width 1,
+        calculate how much water will be retained after it rains.
+        walls is a list of non-negative integers.
         Return: Integer indicating total amount of rainwater retained.
         Assume that the ends of the list (before index 0 and after index
         walls[-1]) are not walls, meaning they will not retain water.
-        If the list is empty return 0.
-    """
+        If the list is empty return 0. """
 
     if walls is None:
         return 0
