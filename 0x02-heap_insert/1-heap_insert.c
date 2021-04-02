26e0a17a148ab36e0337895b9d5fe060c9d3290c
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2021-04-02 16:07:38 -0500

    Task 1

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -8,11 +8,17 @@
  **/
 heap_t *heap_insert(heap_t **root, int value)
 {
-	heap_t *node;
+	heap_t *new_node = NULL;
 
-	if (!root)
+	new_node = malloc(sizeof(*new_node));
+
+	if (!new_node)
 		return (NULL);
-	if (!*root)
-		return (*root = binary_tree_node(NULL, value));
-	return (NULL);
+
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	new_node->parent = *root;
+
+	return (new_node);
 }
