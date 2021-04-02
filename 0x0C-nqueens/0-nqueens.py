ba2311c6dd43f5585c9a0e2d6fd2bcff2b1555ae
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2021-04-02 16:17:20 -0500

    Task 0

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -17,22 +17,25 @@
 import sys
 
 
-def nQueens(n):
-    """ N queens """
-    def recursion():
-        """ Recursive function """
+def nqueens(n: int):
+    """
+    backtracking
+    """
+    matrix = [[0 for x in range(n)] for y in range(n)]
+    print(str(matrix))
 
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
+if __name__ == "__main__":
+    if len(sys.argv) > 2 or len(sys.argv) < 2:
         print("Usage: nqueens N")
         exit(1)
-    try:
-        n = int(sys.argv[1])
-    except:
+
+    if not sys.argv[1].isdigit():
         print("N must be a number")
         exit(1)
+
     if int(sys.argv[1]) < 4:
         print("N must be at least 4")
         exit(1)
-    nQueens(n)
+
+    nqueens(int(sys.argv[1]))
