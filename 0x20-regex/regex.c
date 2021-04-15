879c5da7b77c4e33083d90acaa0c04ae0fadf86d
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2021-04-14 19:19:42 -0500

    Task 0

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -5,24 +5,24 @@
 * @pattern: given pattern
 * Return: 1 if the pattern matches the string, or 0 if it doesn’t.
 */
-
 int regex_match(char const *str, char const *pattern)
 {
-	if (!*pattern)
-	{
-		return (!*pattern);
-	}
-	if (*(pattern + 1) == '*')
-	{
-		if (regex_match(str, pattern + 2))
-			return (1);
-		while (*str && (*pattern == *(str++) || *pattern == '.'))
-		{
-			if (regex_match(str, pattern + 2))
-				return (1);
-		}
-	}
-	else if (*pattern == *str || (*pattern == '.' && *str))
-		return (regex_match(++str, ++pattern));
+	int dot = 0;
+	int asterisk = 0;
+
+	if (!str || !pattern)
+		return (0);
+
+	dot = *str && (*str == *pattern || *pattern == '.');
+	asterisk = *(pattern + 1) == '*';
+
+	if (!*str && !asterisk)
+		return (*pattern ? 0 : 1);
+	else if (dot && asterisk)
+		return (regex_match(str + 1, pattern) || regex_match(str, pattern + 2));
+	else if (dot && !asterisk)
+		return (regex_match(str + 1, pattern + 1));
+	else if (asterisk)
+		return (regex_match(str, pattern + 2));
 	return (0);
 }
