65fcb35c3e99a6eb66a4892d203848818a3193d1
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2021-04-27 11:32:47 -0500

    Task 0

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -22,28 +22,25 @@ def add_prime(n, primes):
 
 
 def isWinner(x, nums):
-    """ x is the number of rounds and nums is an array of n
+    """
+    x is the number of rounds and nums is an array of n
     Return: name of the player that won the most rounds
-    If the winner cannot be determined, return None """
+    If the winner cannot be determined, return None
+    """
     score = {"Maria": 0, "Ben": 0}
     primes = [0, 0, 2]
     add_prime(max(nums), primes)
-
     for round in range(x):
         _sum = sum((i != 0 and i <= nums[round])
                           for i in primes[:nums[round] + 1])
-
         if (_sum % 2):
             winner = "Maria"
         else:
             winner = "Ben"
-
         if winner:
             score[winner] += 1
-
     if score["Maria"] > score["Ben"]:
         return "Maria"
     elif score["Ben"] > score["Maria"]:
         return "Ben"
-
     return None
